108313704e9d56b9c303162f8f43f9d3d4684f3c
Alexey Zhuravlev <zhurus@mail.ru>
2018-09-03 00:36:35 +0300

    All functions aren't in global scope

--- snake-main.js
+++ snake-main.js
@@ -1,15 +1,16 @@
-$(document).ready(function(){
-    // objects.game.state = new objects.State();
-    objects.game = new objects.Game;
-    objects.game.state.draw();
-    objects.game.level = 10;
-    var time = 1000 - (1000 - 100)/10*(objects.game.level-1);
-    setInterval(timeFlows,time);
-});
+(function(){
+    $(document).ready(function(){
+        objects.game = new objects.Game;
+        objects.game.state.draw();
+        objects.game.level = 10;
+        var time = 1000 - (1000 - 100)/10*(objects.game.level-1);
+        setInterval(timeFlows,time);
+    });
 
-function timeFlows(){
-    objects.checkFoodEaten();
-    objects.game.state.snake.move();
-    objects.game.state.updateDrawing();
-    objects.checkGameOver();
-}
+    function timeFlows(){
+        objects.checkFoodEaten();
+        objects.game.state.snake.move();
+        objects.game.state.updateDrawing();
+        objects.checkGameOver();
+    }
+})()
